32c67723759902b42aad623ddd4915b3afa15525
tmdghks7836 <tmdghks0021@gmail.com>
2020-06-20 14:37:46 +0900

    서버 패킷 추가 수정

--- StudyRoomProj/src/packetBase_p/EResult.java
+++ StudyRoomProj/src/packetBase_p/EResult.java
@@ -1,12 +1,11 @@
 package packetBase_p;
 
-
 public enum EResult {
 
-	SUCCESS(1), 
-	NOT_FOUND_DATA(-202),
-	
-	
+	SUCCESS(1), // 성공
+	FAIL(-202), //실패
+	NOT_FOUND_DATA(-1000), // 데이터 찾을 수 없음
+	DUPLICATEED_ID(-1001), // 중복 된 아이디
 	
 	;
 
