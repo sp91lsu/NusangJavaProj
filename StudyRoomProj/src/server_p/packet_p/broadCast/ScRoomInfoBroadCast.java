46dc11a22915078b8d089fcfc1f83f705e50528a
tmdghks7836 <tmdghks0021@gmail.com>
2020-06-24 16:42:46 +0900

    no message

--- StudyRoomProj/src/server_p/packet_p/broadCast/ScRoomInfoBroadCast.java
+++ StudyRoomProj/src/server_p/packet_p/broadCast/ScRoomInfoBroadCast.java
@@ -3,13 +3,17 @@ package server_p.packet_p.broadCast;
 import java.util.ArrayList;
 
 import data_p.product_p.room_p.RoomProduct;
+import packetBase_p.EResult;
 import packetBase_p.PacketBase;
+import packetBase_p.ResultPacketBase;
 
-public class ScRoomInfoBroadCast extends PacketBase {
+public class ScRoomInfoBroadCast extends ResultPacketBase {
 
 	public ArrayList<RoomProduct> roomList;
 
-	public ScRoomInfoBroadCast(ArrayList<RoomProduct> roomList) {
+	public ScRoomInfoBroadCast(EResult eResult, ArrayList<RoomProduct> roomList) {
+		super(eResult);
 		this.roomList = roomList;
+		// TODO Auto-generated constructor stub
 	}
 }
