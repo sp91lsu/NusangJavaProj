54b58d9f96abf6762b794637cc212ba281f52e5f
DESKTOP-7R41BS2\이성웅 <sp91lsu@nate.com>
2020-06-24 15:17:01 +0900

    현재 이용중 고객 리스트 신,액크

---
+++ StudyRoomProj/src/client_p/packet_p/syn_p/MSCurrMemListSyn.java
@@ -0,0 +1,11 @@
+package client_p.packet_p.syn_p;
+
+import packetBase_p.PacketBase;
+
+//현재 이용중 고객 리스트 요청
+public class MSCurrMemListSyn extends PacketBase {
+ 
+	
+	public MSCurrMemListSyn() {
+	}
+}
